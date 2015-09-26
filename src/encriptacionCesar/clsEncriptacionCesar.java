9280006c9e9f8d6b329ad254f1b8368c561b7a4a
alemagno92 <alemagno92@gmail.com>
2015-09-25 21:04:05 -0300

    agregada la primer clase

---
+++ src/encriptacionCesar/clsEncriptacionCesar.java
@@ -0,0 +1,43 @@
+package encriptacionCesar;
+
+import java.lang.reflect.Array;
+import java.util.Vector;
+
+/**
+ * 
+ * @author GildoAlejandro
+ *
+ */
+
+public class clsEncriptacionCesar {
+	private Vector arrayAscii[];
+	private int clave;
+	
+	public clsEncriptacionCesar(int clave){
+		setClave(clave);
+	}
+	//getters
+	private void setClave(int clave){
+		this.clave= clave;
+	}
+	//getters
+	public int getClave(){
+		return this.clave;
+	}
+	//metodos
+	public String encriptarCesar(String mensaje){
+		
+		
+		
+		return texto;
+	}
+	
+	private void generaAscii(String mensaje){
+		for(int index = 0; index < mensaje.length(); index++ ){
+			
+		}
+		
+		
+	}
+	
+}
